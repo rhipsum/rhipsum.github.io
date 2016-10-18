d129e24c8e6ade39e3be30401f65ef4ea9814808
Leonardo Marinho <leonardo.marinho@rgsistemas.com.br>
2016-10-18 16:44:32 -0200

    Deploy

--- js/ipsum.js
+++ js/ipsum.js
@@ -36,7 +36,7 @@ var lib = [
   'A vida é como uma grande firma, a qual despede seus funcionários e contrata novos.',
   'O segredo do sucesso é tratar seus funcionários como seres humanos e não como coisas.',
   'Primeiramente sempre devemos pensar nos funcionários da Empresa, pois sem o motor o carro não se desloca',
-  'Meu lema é confiar. Confiar no freguês, nos fornecedores, nos funcionários, nos amigos e, principalmente, em mim.',
+  'Meu lema é confiar. Confiar no cliente, nos fornecedores, nos colaboradores, nos amigos e, principalmente, em mim.',
   'Os bons funcionários nas empresas não estão ameaçados pelas pessoas más, e sim por aquelas que permitem a maldade no ambiente.',
   'A melhor equipe não conta apenas com os melhores funcionários, mas também com o funcionário certo para a função certa.',
   'A questão essencial não é "o quanto ocupado você está", mas sim "com o que você está ocupado',
